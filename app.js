0f62f6af0bd7ab2342685583984ce8dd59584269
Hamlet Tamazian <hamlettamaz@gmail.com>
2016-03-17 18:45:00 -0700

    working on weird highlighting because of 'numLeft'

--- app.js
+++ app.js
@@ -5,100 +5,89 @@ function swapEls(arr, iA, iB) {
   var temp = arr[iA];
   arr[iA] = arr[iB];
   arr[iB] = temp;
-};
+}
 
 
 var basic = [[1,2,3,4,5,6,7,8,9],[4,5,6,7,8,9,1,2,3],[7,8,9,1,2,3,4,5,6],[2,3,4,5,6,7,8,9,1],[5,6,7,8,9,1,2,3,4],[8,9,1,2,3,4,5,6,7],[3,4,5,6,7,8,9,1,2],[6,7,8,9,1,2,3,4,5],[9,1,2,3,4,5,6,7,8]];
 
+var base = basic;
 
 //grid randomization
 
-var base = basic;
-
-//shift rows
-base.forEach(function (el, i) {
-
-	if (i === 0 || i === 1 || i === 2) {
-		
-		var a = Math.floor(Math.random() * 3);
-		var b = Math.floor(Math.random() * 3);
-
-		swapEls(base, a, b);
-	}
-	else if (i === 3 || i === 4 || i === 5) {
-		var a = Math.floor(Math.random() * 3) + 3;
-		var b = Math.floor(Math.random() * 3) + 3;
-
-		swapEls(base, a, b);
-	}
-	else if (i === 6 || i === 7 || i === 8) {
-		var a = Math.floor(Math.random() * 3) + 6;
-		var b = Math.floor(Math.random() * 3) + 6;
-
-		swapEls(base, a, b);
-	}
-
-});
-
-//shift cols
-base.forEach(function (el, i) {
+function gridRandomization() {
 
 
-	el.forEach(function (val, j){ 
+	//shift rows
+	base.forEach(function (el, i) {
 
-		// var temp = $(`[data-col=${i + 1}]`);
-		// $.makeArray($(`[data-col=${i + 1}]`))
-
-
-		if ($('[data-col') === 0 || $('[data-col') === 1 || $('[data-col') === 2) {
+		if (i === 0 || i === 1 || i === 2) {
 			
 			var a = Math.floor(Math.random() * 3);
 			var b = Math.floor(Math.random() * 3);
 
-			swapEls(el, a, b);
+			swapEls(base, a, b);
 		}
-		else if ($('[data-col') === 3 || $('[data-col') === 4 || $('[data-col') === 5) {
+		else if (i === 3 || i === 4 || i === 5) {
 			var a = Math.floor(Math.random() * 3) + 3;
 			var b = Math.floor(Math.random() * 3) + 3;
 
-			swapEls(el, a, b);
+			swapEls(base, a, b);
 		}
-		else if ($('[data-col') === 6 || $('[data-col') === 7 || $('[data-col') === 8) {
+		else if (i === 6 || i === 7 || i === 8) {
 			var a = Math.floor(Math.random() * 3) + 6;
 			var b = Math.floor(Math.random() * 3) + 6;
 
-			swapEls(el, a, b);
+			swapEls(base, a, b);
 		}
+
 	});
-});
+
+	//shift cols
+	base.forEach(function (el, i) {
 
 
-//switch numbers' places
-[1,2,3,4,5,6,7,8,9].forEach(function (num, idx) {
-		var k = Math.floor(Math.random() * 9) + 1;
-		var temp = num;
-// debugger
-		base.forEach(function(arr) {
-			var temp1 = "fill";
-			arr[arr.lastIndexOf(k)] = temp1;
-			arr[arr.indexOf(temp)] = k;
-			arr[arr.lastIndexOf("fill")] = temp;
+		el.forEach(function (val, j){ 
+
+			// var temp = $(`[data-col=${i + 1}]`);
+			// $.makeArray($(`[data-col=${i + 1}]`))
+
+
+			if ($('[data-col') === 0 || $('[data-col') === 1 || $('[data-col') === 2) {
+				
+				var a = Math.floor(Math.random() * 3);
+				var b = Math.floor(Math.random() * 3);
+
+				swapEls(el, a, b);
+			}
+			else if ($('[data-col') === 3 || $('[data-col') === 4 || $('[data-col') === 5) {
+				var a = Math.floor(Math.random() * 3) + 3;
+				var b = Math.floor(Math.random() * 3) + 3;
+
+				swapEls(el, a, b);
+			}
+			else if ($('[data-col') === 6 || $('[data-col') === 7 || $('[data-col') === 8) {
+				var a = Math.floor(Math.random() * 3) + 6;
+				var b = Math.floor(Math.random() * 3) + 6;
+
+				swapEls(el, a, b);
+			}
 		});
-});
-
-// base.forEach(function (el, i) {
+	});
 
 
-// 	el.forEach(function (val, j){ 
-		
-// 		
-// 			var temp = val;
-// 			val = k;
-
-
-// 		}
-// 	});
-// });
+	//switch numbers' places
+	[1,2,3,4,5,6,7,8,9].forEach(function (num, idx) {
+			var k = Math.floor(Math.random() * 9) + 1;
+			var temp = num;
+	// debugger
+			base.forEach(function(arr) {
+				var temp1 = "fill";
+				arr[arr.lastIndexOf(k)] = temp1;
+				arr[arr.indexOf(temp)] = k;
+				arr[arr.lastIndexOf("fill")] = temp;
+			});
+	});
+}
 
 
 // debugger
@@ -111,18 +100,18 @@ var difficulty = 0.015;
 var gCount = 0;
 
 
-$('#difficulty').on('click', 'button', chooseD);
+$('#difficulty').on('click', 'button', chooseDif);
 
-function chooseD(e) {
+function chooseDif(e) {
 	// debugger
 	if (e.target.innerText === "Easy") {
-		difficulty = 0.7;
+		difficulty = 0.55;
 	}
 	else if (e.target.innerText === "Medium") {
-		difficulty = 0.5;
+		difficulty = 0.65;
 	}
 	else if (e.target.innerText === "Hard") {
-		difficulty = 0.3;
+		difficulty = 0.75;
 	}
 
 	$('#difficulty').css('display', 'none');
@@ -140,6 +129,7 @@ $('#reset').on('click', reset);
 
 function reset(e) {
 	localStorage.removeItem("High Scores");
+	settingHS();		
 }
 
 //setting HS in list
@@ -200,16 +190,21 @@ function timer() {
 
 //create the deck
 function sudokuGen() {
+	//grid setup
+	gridRandomization();
 	//reset game score
 	nScore = {};
 	//clears the field of initial sudoku grid
-$('#Mtbody td').empty();
+	$('#Mtbody td').empty();
 	//remove "perm" class from all td's before adding new settings
-$.makeArray($('#Mtbody td')).forEach(function(el, i) { $(el).removeClass("perm"); });
+	$.makeArray($('#Mtbody td')).forEach(function(el, i) { $(el).removeClass("perm"); });
 	//reset timer
-(function() { h1.textContent = "00:00:00"; seconds = 0; minutes = 0; hours = 0;})();
+	(function() { h1.textContent = "00:00:00"; seconds = 0; minutes = 0; hours = 0;})();
 	//set background white
-$('td').css("background-color", "white");
+	$('td').css("background-color", "white");
+	//turn on inputRow event lister
+	$('#inputRow').on("click", "td", numPick);
+
 
 // debugger
 	base.forEach(function(el_arr, i){
@@ -227,15 +222,30 @@ $('td').css("background-color", "white");
 //gets rid of difficulty section if you didn't choose a difficulty when starting 
 	$('#difficulty').css('display', 'none');
 
+	numsLeft();
 }
 
+function numsLeft() {
+	$.makeArray($('.numsLeft')).forEach(
+		function(el, i) {
+			// debugger
+			var inputVal = el.parentNode.innerText[0];
+			var howMany = $.makeArray($('#Mtbody td')).filter(function(el, i) {
+				return $(el).text() === inputVal;
+			});
+			howMany = howMany.length;
+			$(el).text(howMany);
+		});
+}
 
-//cell high-lighting
-// $('#eraser').on('click', cancelHl);
+//ERASER cell high-lighting
+$('#eraser').on('click', cancelHl);
+
 
 function cancelHl(e) {
 	// $('#Mtbody').off('click', cancelHl);
 	$('td').css("background-color", "white");
+
 }
 
 
@@ -248,7 +258,7 @@ function highlight(e) {
 	var n_row = Number(e.target.dataset.row);
 	var n_col = Number(e.target.dataset.col);
 	var n_grp = Number(e.target.dataset.grp);
-	var yellow = "rgb(255, 255, 0)";
+	var yellow = "rgb(247, 249, 143)";
 
 	var $row = $(`[data-row=${n_row}]`);
 	var $col = $(`[data-col=${n_col}]`);
@@ -261,11 +271,21 @@ function highlight(e) {
 	else {
 		$('td').css("background-color", "white");
 		
-		$row.css("background-color", "yellow");
-		$col.css("background-color", "yellow");
-		$grp.css("background-color", "yellow");
+		$row.css("background-color", "rgb(247, 249, 143)");
+		$col.css("background-color", "rgb(247, 249, 143)");
+		$grp.css("background-color", "rgb(247, 249, 143)");
 	}
 
+	if ($(e.target).text()) {
+		var num = $(e.target).text();
+		$.makeArray($('#Mtbody td')).forEach(numBold);
+
+		function numBold(el, i) {
+			if (num === $(el).text()) {
+				$(el).css("background-color", "rgba(143, 194, 249, 0.6)");
+			}
+		}
+	}
 }
 
 //highlight the number you choose
@@ -274,7 +294,7 @@ $('#inputRow').on('click', 'td', numHl);
 function numHl(e) {
 // debugger
 	// $('#inputRow').css('background-color', 'white');
-	$(e.target).css("background-color", "yellow");
+	$(e.target).css("background-color", "rgb(247, 249, 143)");
 	setTimeout(function() {
 		$(e.target).css("background-color", "white");
 	}, 1000);
@@ -283,12 +303,17 @@ function numHl(e) {
 
 
 //create the input numbers
-$('#inputRow').on("click", numPick);
+// $('#inputRow').on("click", numPick);
 
 
 function numPick (e) {
-	// debugger
-	var num = e.target.innerText;
+	debugger
+	if (this.innerText[0]) {
+		var num = this.innerText[0];	
+	}
+	else {
+		var num = "  ";
+	}
 
 	
 	$('#Mtbody').on('click', check);
@@ -296,9 +321,14 @@ function numPick (e) {
 
 	function numAsn (e) {
 		// debugger
+
+		//check if cell clicked isn't "perm" and we're not clicking the same thing
 		if (e.target.className.indexOf("perm") === -1 && e.target.innerText !== num) {
+			//insert number in cell
 			$(e.target).text(num);	
 		}
+		//check how many of each number is left
+		numsLeft();
 		$('#Mtbody').off('click', numAsn);
 		
 		//figuring out when game ends
@@ -327,39 +357,47 @@ function numPick (e) {
 		}
 
 		if (gameEndCheck()) {
-			// alert("YOU WIN!!");
 
-			
-			(function() {
-    		clearTimeout(t);})();
+			//stops time
+    	clearTimeout(t);
 		
-
-
-			//storing the win times in storage & displaying in high score list
-			var name = prompt("What is your name?");
-
-			nScore.name = name;
-			nScore.time = $('time').text();
-
-			hsArray.push(nScore);
-
-				localStorage.setItem("High Scores", JSON.stringify(hsArray));
-
-			//print to HS list
-			settingHS();
-
 			//winning Banner
-			// var winBanner = "<p id='winner'>You Win!!!</p>";
-			// $('body').append(winBanner);
 			$('#winner').css('visibility', 'visible');
-			// debugger
-			// $('body').on("click", function() {$('#winner').css('visibility', 'hidden');});
-
-			//turn "play" button back on
-			$('#startGame').on('click', initialize);
-
-	}
+			$('#winner').on("click", winner); 
+		}
 }
+
+function winner(e) {
+	//hides winner banner
+
+	//asks for player name and set high score
+	var name = prompt("What is your name?") || "Guest";
+	$('#winner').css('visibility', 'hidden');
+	//hides any highlighting
+	$('td').css("background-color", "white");
+	hsArray.push(nScore);
+	
+
+	//storing the win times in storage & displaying in high score list
+
+	nScore.name = name;
+	nScore.time = $('time').text();
+
+
+		localStorage.setItem("High Scores", JSON.stringify(hsArray));
+
+	//print to HS list
+	settingHS();
+
+
+	//turns off event listeners of the inputRow and Body
+	$('#inputRow').off("click", numPick);
+	$('#winner').off("click", winner);				
+
+
+	//turn "play" button back on
+	$('#startGame').on('click', initialize);
+	}
 
 
 // n stands for number (ie. row number)
